754696fd558c06175396c0f4fa0be986bf09e8d0
Erik Hatfield <erikhatfield@Eriks-MacBook-Pro.local>
2019-12-27 22:47:01 -0600

    test inprog

--- react-client/src/components/headerComponent/navBar.js
+++ react-client/src/components/headerComponent/navBar.js
@@ -1,16 +1,64 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import logo from './test.jpg';
 class NavBar extends Component {
+  
   render() {
+    
     return (
-      <header>
-        <ul id="headerButtons">
-          <li className="navButton"><Link to="">Home</Link></li>
-        </ul>
+      <header className="header1">
+        <Container className="container">
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+          >
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <Container className="smallContainer2">
+            <img className="image" src={logo}></img>
+          </Container>
+          <Container className="smallContainer">
+            BAWK
+          </Container>
+        </Grid>
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+            <Container className="navContainer">
+              <Link to="">TEST TEST</Link>
+            </Container>
+            
+            <Container className="navContainer">
+              <Link to="">TEST TEST</Link>
+            </Container>
+            <Container className="navContainer">
+              <Link to="">TEST TEST TEST</Link>
+            </Container>
+          </Grid>
+        </Grid>
+        </Container>
       </header>
+      // <header>
+      //   <Navbar expand="lg" variant="light" bg="light">
+      //     <Navbar.Brand href="#">Navbar</Navbar.Brand>
+      //   </Navbar>
+      // </header>
     )
   }
 }
+
+
 
 export default NavBar;
--- react-client/src/components/pages/homePage.js
+++ react-client/src/components/pages/homePage.js
@@ -1,13 +1,96 @@
 import React, { Component } from 'react';
+import Columns from 'react-columns';
+//import { Card, CardContent, makeStyles, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import SimpleCard from '../cards/simpleCard';
+
+
 
 class HomePage extends Component {
   render() {
     return (
-      <div>
+      <div style={{backgroundColor:"lightblue"}}>
         Home page
+        <Grid
+        container
+        direction="col"
+        justify="center"
+        alignItems="center"
+        spacing={1}
+      >
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          spacing={1}
+        >
+          <Grid item xs={3}>
+            <Container className="homeNavButton">
+              ONE
+            </Container>
+          </Grid>
+          <Grid item xs={3}>
+            <Container className="homeNavButtonAlt">
+              TWO
+            </Container>
+          </Grid>
+          <Grid item xs={3}>
+            <Container className="homeNavButton">
+              ONE
+            </Container>
+          </Grid>
+          <Grid item xs={3}>
+            <Container className="homeNavButtonAlt">
+              TWO
+            </Container>
+          </Grid>
+        </Grid>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        spacing={1}
+      >
+        <Grid item xs={8}>
+          <Container className="medHome">
+            ONE
+          </Container>
+        </Grid>
+        <Grid item xs={4}>
+          <Container className="smallHome">
+            TWO
+          </Container>
+        </Grid>
+      </Grid>
+      </Grid>
       </div>
     )
   }
 }
 
 export default HomePage;
+
+
+{/* <div className="container" style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh', width: '100vh'}}>
+          <Container>
+            
+          </Container>
+          
+          <Container>
+            <Button variant="contained" color="secondary">
+              Join
+            </Button>
+          </Container>
+        </div>
+          <h3>This is a heading</h3>
+          <p>This is a paragraph.</p>
+      </div> */}
